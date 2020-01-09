4e1ec2c0610c06872ea91da0ade1f9f4910e2f08
ShadowNK <44003612+ShadowNK@users.noreply.github.com>
2020-01-09 11:03:40 -0500

    Add files via upload

---
+++ Chat 1-1 distintos equipos/clientSocket.py
@@ -0,0 +1,24 @@
+from socket import *
+# Creating a clientsocket
+
+# Variables
+HOST = '172.31.108.20'  # Direccion IP del servidor
+PORT = 5000
+server = (HOST, PORT)
+
+# Crear el socket
+sock = socket(AF_INET, SOCK_STREAM)
+
+# Establecer coneccion
+sock.connect(server)
+
+# Comunicacion
+while True:
+    message = input("Mensaje: ")
+    sock.send(message.encode())
+    print ("Esperando respuesta ...")
+    reply = sock.recv(1024)
+    print ("Recivido: ", repr(reply.decode()))
+    
+# Cerrar coneccion
+sock.close()
---
+++ Chat 1-1 distintos equipos/serverSocket.py
@@ -0,0 +1,26 @@
+from socket import *
+# Creating a server socket on local machine
+
+# VARIABLES
+HOST = '172.31.108.20'  # Direccion IP del seridor
+PORT =  5000
+
+# Socket
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind( (HOST, PORT) )
+sock.listen(1)
+
+# Establecer la coneccion
+conn, addr = sock.accept()
+print ("Conectado con: ", addr)
+
+# Comunicacion
+while True:
+    data = conn.recv(1024)
+    print ("Recivido: ", repr(data.decode()))
+    reply = input("Respuesta: ")
+    conn.sendall(reply.encode())
+    print ("Esperando respuesta ...")
+
+# Cerrar la coneccion
+conn.close()
