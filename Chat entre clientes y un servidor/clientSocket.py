a5c1fe47106af4dc3540b4f7120c6ec886468630
ShadowNK <44003612+ShadowNK@users.noreply.github.com>
2020-01-14 11:46:07 -0500

    Add files via upload

---
+++ Chat entre clientes y un servidor/clientSocket.py
@@ -0,0 +1,44 @@
+from socket import *
+from _thread import *
+# Creating a clientsocket
+
+# Variables
+HOST = 'localhost'  # Direccion IP del servidor
+PORT = 50010
+server = (HOST, PORT)
+
+# Crear el socket
+sock = socket(AF_INET, SOCK_STREAM)
+
+# Establecer coneccion
+sock.connect(server)
+
+def get_ip():
+    s = socket(AF_INET, SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def reciver():
+    reply = sock.recv(1024)
+    print ("Recivido: \n", repr(reply.decode()))
+
+
+def sender():
+    message = input("Mensaje: ")
+    message = get_ip() + ': ' + message+ '\n'
+    sock.send(message.encode())
+    
+
+# Comunicacion
+while True:
+    start_new_thread(sender,())
+    start_new_thread(reciver,())
+
+# Cerrar coneccion
+sock.close()
---
+++ Chat entre clientes y un servidor/serverSocket.py
@@ -0,0 +1,39 @@
+from socket import *
+from _thread import *
+
+# VARIABLES
+HOST = 'localhost'  # Direccion IP del seridor
+PORT =  50010
+server = (HOST, PORT)
+
+device = 0
+dev = [] 
+
+
+def on_new_client(clientsocket,addr):
+    global device, dev
+    dev.append((clientsocket,addr))
+    device += 1
+    print ("Conectado con: ", addr)
+    while True:
+        msg = clientsocket.recv(1024)
+        resend_msg(msg, addr)
+        
+    clientsocket.close()
+
+def resend_msg(msg, ad):
+    for i in range(0, device):
+        (clientsocket,addr) = dev[i]
+        if ad != addr:
+            clientsocket.send(msg)
+
+# SOCKET
+sock = socket()                 # Create a socket object
+sock.bind(server)               # Bind to the port
+sock.listen(5)                  # Number of connections
+
+
+while True:
+   conn, addr = sock.accept()     # Establish connection with client.
+   start_new_thread(on_new_client,(conn,addr))
+sock.close()
